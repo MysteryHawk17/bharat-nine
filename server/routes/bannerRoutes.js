dca6beb75b9cd382184e1ffa6210bd2d9ccb84ee
Yash Raj <raj.yash1217@gmail.com>
2023-05-26 14:32:43 +0530

    minor bugs fixed

--- server/routes/bannerRoutes.js
+++ server/routes/bannerRoutes.js
@@ -1,11 +1,12 @@
 const { addBanner, deleteBanner } = require("../controllers/bannerController");
-
+const {checkAdmin}=require("../middlewares/authMiddleware");
 const router=require("express").Router();
+const upload=require("../utils/multer");
 
 
 
-router.post("/create",addBanner);
-router.delete("/delete/:id",deleteBanner);
+router.post("/create",checkAdmin,upload.single('image'),addBanner);
+router.delete("/delete/:id",checkAdmin,deleteBanner);
 
 
 module.exports=router;
