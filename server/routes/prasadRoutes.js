806ca86616f8c159ac8a55da3628814fe62b2364
Yash Raj <raj.yash1217@gmail.com>
2023-06-01 13:34:19 +0530

    auth check added

--- server/routes/prasadRoutes.js
+++ server/routes/prasadRoutes.js
@@ -4,12 +4,12 @@ const router = require("express").Router();
 const upload = require("../utils/multer")
 
 
-router.post("/createprasad", upload.single('image'), createPrasad);
-// router.post("/createprasad",checkAdmin,upload.single('image'),createPrasad);
+// router.post("/createprasad", upload.single('image'), createPrasad);
+router.post("/createprasad",checkAdmin,upload.single('image'),createPrasad);
 router.get("/getallprasad", getAllPrasad);
 router.get("/getprasad/:id", getPrasad);
 router.delete("/deleteprasad/:id", checkAdmin, deletePrasad);
-router.put("/updateprasad/:id", upload.single('image'), updatePrasad);
-// router.put("/updateprasad/:id",checkAdmin,upload.single('image'),updatePrasad);
+// router.put("/updateprasad/:id", upload.single('image'), updatePrasad);
+router.put("/updateprasad/:id",checkAdmin,upload.single('image'),updatePrasad);
 
 module.exports = router;
