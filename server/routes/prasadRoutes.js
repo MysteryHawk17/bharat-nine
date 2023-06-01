5ecdd2db533a84169e304277bbc1ff34a83746bc
Yash Raj <raj.yash1217@gmail.com>
2023-06-01 12:32:51 +0530

    admin check removed from prasad routes

--- server/routes/prasadRoutes.js
+++ server/routes/prasadRoutes.js
@@ -1,13 +1,15 @@
-const {  createPrasad, getAllPrasad, getPrasad, deletePrasad, updatePrasad } = require("../controllers/prasadController");
-const {checkAdmin}=require("../middlewares/authMiddleware");
-const router=require("express").Router();
-const upload=require("../utils/multer")
+const { createPrasad, getAllPrasad, getPrasad, deletePrasad, updatePrasad } = require("../controllers/prasadController");
+const { checkAdmin } = require("../middlewares/authMiddleware");
+const router = require("express").Router();
+const upload = require("../utils/multer")
 
 
-router.post("/createprasad",checkAdmin,upload.single('image'),createPrasad);
-router.get("/getallprasad",getAllPrasad);
-router.get("/getprasad/:id",getPrasad);
-router.delete("/deleteprasad/:id",checkAdmin,deletePrasad);
-router.put("/updateprasad/:id",checkAdmin,upload.single('image'),updatePrasad);
+router.post("/createprasad", upload.single('image'), createPrasad);
+// router.post("/createprasad",checkAdmin,upload.single('image'),createPrasad);
+router.get("/getallprasad", getAllPrasad);
+router.get("/getprasad/:id", getPrasad);
+router.delete("/deleteprasad/:id", checkAdmin, deletePrasad);
+router.put("/updateprasad/:id", upload.single('image'), updatePrasad);
+// router.put("/updateprasad/:id",checkAdmin,upload.single('image'),updatePrasad);
 
-module.exports=router;
+module.exports = router;
